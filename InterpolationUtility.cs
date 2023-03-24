53ff2b36c58f683b1609257c7fdf15bdec2febeb
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-03-25 00:56:46 +0700

    1.0.1

---
+++ InterpolationUtility.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Interpolation
+{
+    public static class InterpolationUtility
+    {
+        public static float EaseInBack(float val, float period = 1.70158f)
+        {
+            var s = period + 1;
+            return s * val * val * val - period * val * val;
+        }
+
+        public static float EaseOutBack(float val, float period = 1.70158f)
+        {
+            var s = period + 1;
+            return (float)(1 + s * Math.Pow(val - 1, 3) + period * Math.Pow(val - 1, 2));
+        }
+
+        public static float Linear(float value)
+        {
+            return value;
+        }
+    }
+}
